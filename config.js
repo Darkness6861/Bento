9b9a17a2d87c750a571b5f8a351e9874b50233e0
Darkness6861 <113201935+Darkness6861@users.noreply.github.com>
2022-09-10 14:22:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '122001be2927bb1de8228322b25a2d47', // Write here your API Key
-	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
